e9d4534bf192c05a0c094955f4ee3efc4052521b
Peter Czigany <p.czigany@gmail.com>
2017-07-06 10:52:22 +0200

    Add initial commit

---
+++ src/main/java/com/czipeter/prio/PrioApplication.java
@@ -0,0 +1,12 @@
+package com.czipeter.prio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrioApplication.class, args);
+	}
+}
